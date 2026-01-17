7f7067a2d330b7c4d151e7b9fc365361bc1a5d22
Bratka78 <maksymbrat@gmail.com>
2026-01-18 00:43:25 +0100

    first commit

---
+++ task_1_hw_4.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+def total_salary(path: str) -> tuple:
+    total = 0
+    count = 0
+    try:
+        with open(path, "r", encoding="utf-8") as fh:
+            try:
+                for i in fh:
+                    key, value = i.strip().split(",")
+                    total += int(value)
+                    count += 1
+            except ValueError:
+                    print(f"Error in line: {i.strip()} .")
+                    return 0,0
+            if count == 0:
+                print("Empty file")
+                return 0,0
+            average_salary = round(total / count, 2)
+            return total, average_salary
+        
+    except FileNotFoundError:
+        print("File not found")
+        return 0,0
+            
+
+cont = total_salary(r"C:\Users\Maksym\Desktop\my_projects\homework\goit_algo_hw_04\price.txt")
+print(cont)
---
+++ task_2_hw_4.py
@@ -0,0 +1,29 @@
+
+
+def get_cats_info(path):
+    cats = []
+    try:
+        with open(path,'r', encoding="utf-8") as pets:
+            for i in pets:
+                animal = i.strip().split(",")
+                if len(animal) != 3:
+                    continue
+                id, name, age = animal
+                if not age.isdigit():
+                    continue
+                cats.append({
+                    "id": id,
+                    "name": name,
+                    "age": age
+                })
+              
+        return cats
+    except FileNotFoundError:
+        print("File not found")
+        return []
+
+
+
+
+cont = get_cats_info(r"C:\Users\Maksym\Desktop\my_projects\homework\goit_algo_hw_04\pats_info.txt")
+print(cont)
